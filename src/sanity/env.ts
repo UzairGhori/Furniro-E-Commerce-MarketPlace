3b3e59b2ca7cffa19ac58016a3a79c765a81d8e9
Your Name <you@example.com>
2025-02-08 13:23:50 +0500

    first commit

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -8,8 +8,9 @@ export const dataset = assertValue(
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+  "Missing or invalid environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+);
+
 
 export const token = assertValue(
   "skYSYOZhhnDvO9z40OBB9hh7WYpwysX3Xv13l7OqfP5aFqY8E3uvNDPIVY0WL6kSNkTB6UDiKXa7Twp4MjX6bC08sGR5j3nFCZ6IGfsbbMlzTx6edEkprpvRfxFfSObD08NCtuexco3FmaHZY8W7q3v0q69jGscl77T7WCIeV1iVsA3PdLE3",
